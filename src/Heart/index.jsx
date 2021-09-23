d22cfdeb1890baedfb379189bbbd495aef3aeefa
Stephen <farrenzo47@gmail.com>
2021-09-23 18:28:20 -0400

    Hide heart

--- src/Heart/index.jsx
+++ src/Heart/index.jsx
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types';
+
 const Heart = (props) => {
+    const { shouldShow } = props;
     return (
-        <div className='py-4 flex justify-center'>
+        <div className={`py-4 flex justify-center ${!shouldShow && 'hidden'}`}>
             <span>Made with ❤️ by <a className='text-blue-500' href='https://www.recapped.io'>Recapped.io</a></span>
         </div>
     )
 }
+
+Heart.propTypes = {
+    shouldShow: PropTypes.bool.isRequired,
+};
 
 export default Heart;
