f6e172a8a0d0d1fa0f3973c6c257f3cf52bdfb23
Stephen <farrenzo47@gmail.com>
2021-09-23 20:28:43 -0400

    Heart copy edits

--- src/Heart/index.jsx
+++ src/Heart/index.jsx
@@ -4,11 +4,11 @@ const Heart = (props) => {
     const { shouldShow } = props;
     return (
         <div className={`p-4 h-screen relative ${!shouldShow && 'hidden'}`}>
-            <div className='flex flex-col justify-center items-center'>
+            <div>
                 <p className='text-sm text-gray-700 py-2'>We realize this may be your first time in the Bay Area, so the Recapped team made this simple curated guide of our favorites spots.</p>
-                <p className='text-sm text-gray-700 py-2'>We believe giving as much value as we possibly can.</p>
-                <p className='text-sm text-gray-700 py-2'>Recapped.io is the easiest way for sales and success teams to collaborate with their customers on complex deals. If you struggle with a lengthy sales cycle, we'd love to help you out.</p>
-                <span className='text-gray-700 absolute bottom-40'>Made with ❤️ by <a className='text-blue-500' href='https://www.recapped.io'>Recapped.io</a></span>
+                <p className='text-sm text-gray-700 py-2'><a className='text-blue-500' href='https://www.recapped.io'>Recapped.io</a> is the easiest way for sales and success teams to collaborate with their customers on complex deals. If you struggle with a lengthy sales cycle, we'd love to help you out.</p>
+                <p className='text-sm text-gray-700 py-2'>See you around SaaStr!</p>
+                <p className='text-sm text-gray-700 py-2 text-right'>- ❤️ the Recapped Team</p>
             </div>
         </div>
     )
