2700c69170035f2343c0cc65057a02dd9afeef3a
Stephen <farrenzo47@gmail.com>
2021-09-26 13:55:32 -0400

    Updated header

--- src/RecappedBanner/index.jsx
+++ src/RecappedBanner/index.jsx
@@ -1,7 +1,7 @@
 const RecappedBanner = (props) => {
     return (
         <div className='lg:px-32 flex justify-center'>
-            <img className='' alt='SaaStrmap curated by Recapped' src={process.env.PUBLIC_URL + '/Header - Max Width.png'} />
+            <img className='' alt='SaaStrmap curated by Recapped' src={process.env.PUBLIC_URL + '/SaaStr Map Header.png'} />
         </div>
     )
 }
